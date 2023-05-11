de4e273764ff46663e939e798998193292253a51
HieuT23511 <hieut23511@gmail.com>
2023-05-11 15:57:42 +0700

    Time CountDown

--- c.TimeCountDown/main.js
+++ c.TimeCountDown/main.js
@@ -4,10 +4,9 @@ function countdown(){
         clicked = true;
         let inputNumber = document.getElementById('inputValue').value;
         let result = document.getElementById(`result`);
-        result.innerHTML = inputNumber;
         let myInterval = setInterval(()=>{
-            result.innerHTML = inputNumber--
-            if(inputNumber<= 0){
+            result.innerHTML = `${inputNumber--}`
+            if(inputNumber < 0 ){
                 clearInterval(myInterval)
                 result.innerHTML = `Counter Finished!`
                 clicked = false;
